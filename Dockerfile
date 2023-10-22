fa93650368915cea4b95593c62076b48e083f27a
TRO <timo.ranta-ojala@taidekaupunki.fi>
2023-10-22 19:48:02 +0300

    eka kommitointi

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+D ockerfile:
+
+FROM denoland/deno:alpine-1.37.0
+
+EXPOSE 7777
+
+WORKDIR /app
+
+COPY deps.js .
+
+RUN deno cache deps.js
+
+COPY . .
+
+CMD [ "run", "--allow-net", "--watch", "--unstable", "app.js" ]
---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+import { serve } from "./deps.js";
+
+const handleRequest = (request) => {
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
+};
+
+serve(handleRequest, { port: 7777 });
